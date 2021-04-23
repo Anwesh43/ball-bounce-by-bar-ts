3d4bb1f1a3af440e127b785e6837512c31993972
Anwesh43 <anweshthecool0@gmail.com>
2021-04-23 18:54:53 +0530

    adding BBLNode class

--- index.ts
+++ index.ts
@@ -140,3 +140,47 @@ class Animator {
         }
     }
 }
+
+class BBLNode {
+
+    state : State = new State()
+    next : BBLNode
+    prev : BBLNode 
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BBLNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBBLNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BBLNode {
+        var curr : BBLNode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+    
+
+}
